2d7929a52046fd8add7db533000d591311b5636c
littleflute <13699175164@sina.cn>
2017-06-23 14:00:45 -0500

    Add files via upload

---
+++ Depeche Mode/Some Great Reward [Rhino US CD-DVD] Disc 1/m.js
@@ -0,0 +1,11 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Something to Do.mp3";		
+f[2]	= "02 Lie to Me.mp3";		
+f[3]	= "03 People Are People.mp3";		
+f[4]	= "04 It Doesn't Matter.mp3";		
+f[5]	= "05 Stories of Old.mp3";		
+f[6]	= "06 Somebody.mp3";		
+f[7]	= "07 Master and Servant.mp3";		
+f[8]	= "08 If You Want.mp3";		
+f[9]	= "09 Blasphemous Rumours.mp3";		
